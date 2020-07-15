6b0731d6061c07234a3a35b56e7bed6f2f25cadb
Maria Jaramillo <milagrosmjr20@gmail.com>
2020-07-14 21:40:15 -0400

    config img in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
-    resolve: {
-        extensions: ['.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin(
-            {
-                inject: true,
-                template: './public/index.html',
-                filename: './index.html'
-            }
-        ),
-        new CopyWebpackPlugin({
-           patterns: [{ from: './src/styles/styles.css',
-            to: '' }]
-        })
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  resolve: {
+    extensions: [".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/styles/styles.css", to: "" }]
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./public/img/", to: "img/" }]
+    })
+  ]
 }
